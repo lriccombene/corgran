0fbf0476abfd556039f800abe5f4ec9b6fb83793
lriccombene <lriccombene@gmail.com>
2018-07-22 15:10:03 -0300

    graba y actualiza

--- clases/delegacion.php
+++ clases/delegacion.php
@@ -20,13 +20,15 @@
  */
 class delegacion {
     public $id_delegacion;
+    public $delegacion;
     public $localidad; //directoria o ruta de archivo
     public $mes ;
     public $anio;
     public $id_usuario;
     
-    public function __construct($id_delegacion,$localidad,$mes,$anio,$id_usuario) {
+    public function __construct($id_delegacion,$delegacion,$localidad,$mes,$anio,$id_usuario) {
             $this->id_delegacion=$id_delegacion;
+            $this->delegacion=$delegacion;
             $this->localidad=$localidad;
             $this->mes=$mes;
             $this->anio=$anio;
@@ -34,21 +36,46 @@ class delegacion {
     } 
     
     public function guardar_delegacion($obj_delegacion){
+		echo("holaaa estoy en guardar");
+	
+		$servername = "localhost";
+		$username = "root";
+		$password = "slam2018";
+		$dbname = "corgran";
 
-        // Create connection
-        $conn = $obj_coneccion->conectar();
+			// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+			// Check connection
+		if ($conn->connect_error) {
+		        die("Connection failed: " . $conn->connect_error);
+		} 
+		//----busca si existe para saber que query ejecutar insert o update 
+		$sql= "SELECT * FROM `delegacion` WHERE id_usuario='$obj_delegacion->id_usuario'";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+		        // output data of each row
+			$sql = "Update `delegacion` set delegacion='$obj_delegacion->delegacion',localidad='$obj_delegacion->localidad', mes='$obj_delegacion->mes',
+                anio='$obj_delegacion->anio' where id_usuario ='$obj_delegacion->id_usuario'";
+		}else {
+        $sql = "INSERT INTO `delegacion` (delegacion,localidad, mes, anio,id_usuario)
+        			VALUES ('$obj_delegacion->delegacion','$obj_delegacion->localidad','$obj_delegacion->mes', 
+        			'$obj_delegacion->anio','$obj_delegacion->id_usuario')";
         
-        $sql = "INSERT INTO delegacion (localidad, mes, anio,id_usuario)
-        VALUES ('$obj_delegacion->localidad','$obj_delegacion->mes', '$obj_delegacion->anio','$obj_delegacion->id_usuario')";
-        
-        if ($conn->query($sql) === TRUE) {
-            $result = $conn->lastInsertRowID();
-        } else {
+		}
+		
+      if ($conn->query($sql) === TRUE) {
+            $result = "OK";
+      } else {
             $result = "Error: " . $sql . "<br>" . $conn->error;
-        }
-        
+      }
         $conn->close();
-        return $result;
+		echo "llegue al final";        
+       
+        
+        
+        
+        
+        
 
     }
     public function borrar_delegacion($id_usuario){
@@ -75,7 +102,7 @@ class delegacion {
         $result=[];
         while ($fila = mysql_fetch_assoc($resultado)) {
             
-            $obj_delegacion=new delegacion($fila["id_delegacion"],$fila["localidad"],$fila["mes"],
+            $obj_delegacion=new delegacion($fila["id_delegacion"],$fila["delegacion"],$fila["localidad"],$fila["mes"],
                                           $fila["anio"],$fila["id_usuario"]);
             $result[]=$obj_delegacion;
         }
@@ -87,7 +114,7 @@ class delegacion {
         // Create connection
         $conn = $obj_coneccion->conectar();
         
-        $sql = "Update delegacion set localidad='$obj_delegacion->localidad', mes='$obj_delegacion->mes',
+        $sql = "Update delegacion set delegacion='$obj_delegacion->delegacion',localidad='$obj_delegacion->localidad', mes='$obj_delegacion->mes',
                 anio='$obj_delegacion->anio' where id_usuario ='$obj_delegacion->id_usuario'";
         
         mysqli_query($conn,$sql) or die(mysqli_error($conn));
